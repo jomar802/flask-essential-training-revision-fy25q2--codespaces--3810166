b84e52e147aad07a817384d37979ea4012608296
jomar802 <infamousj2013@gmail.com>
2024-11-24 08:05:55 -0600

    First 2 exercises done

--- 3-2-view-functions/starter/app.py
+++ 3-2-view-functions/starter/app.py
@@ -11,5 +11,12 @@ posts = {
 def home():
     return '<h1>Welcome to My Blog</h1><p>Click on the posts to learn more about Flask.</p>'
 
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    post = posts.get(post_id)
+    if not post:
+        return '<h1>404-->Post not found or invalid access</h1>'
+    return f'<h1>{post['title']}</h1> <p>{post['content']}</p>'
+
 if __name__ == '__main__':
     app.run(debug=True)
