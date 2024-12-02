d79efeaedddd1d2a182a7f5b2915825601691c35
jomar802 <infamousj2013@gmail.com>
2024-12-02 15:45:00 -0600

    Added [6-1]

--- 6-1-db-setup/starter/app.py
+++ 6-1-db-setup/starter/app.py
@@ -1,8 +1,24 @@
 from flask import Flask, render_template, request
 from forms import MyForm
+from flask_sqlalchemy import SQLAlchemy
+
+
 
 app = Flask(__name__)
 app.secret_key = 'supersecretkey'  
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///health_tracker.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), nullable=False, unique=True)
+    email = db.Column(db.String(150), nullable=False, unique=True)
+
+def __repr__(self):
+    return f'<User(self.username)>'
 
 @app.route('/form', methods=['GET', 'POST'])
 def form():
