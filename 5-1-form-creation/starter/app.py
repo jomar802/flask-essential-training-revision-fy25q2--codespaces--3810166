c434f45f046a8ed7e1fdb3f3c5e1dde35028e6b1
jomar802 <infamousj2013@gmail.com>
2024-12-02 12:18:52 -0600

    Added [5-1] form.py, app.py, and form html

--- 5-1-form-creation/starter/app.py
+++ 5-1-form-creation/starter/app.py
@@ -1,7 +1,17 @@
 from flask import Flask, render_template
+from forms import MyForm
 
 app = Flask(__name__)
+app.secret_key = 'supersecterkey'
 
+@app.route('/form', methods=['GET','POST'])
+def form():
+    form = MyForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        email = form.email.data
+        return f"Hello, {username}! We've received your email: {email}!"
+    return render_template('form.html', form=form)
 
 
 if __name__ == '__main__':
