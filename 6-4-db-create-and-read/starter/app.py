4e9c1a15d7e5878bb06ad0be4056416ed3463748
jomar802 <infamousj2013@gmail.com>
2024-12-04 07:14:52 -0600

    Added 6-4

--- 6-4-db-create-and-read/starter/app.py
+++ 6-4-db-create-and-read/starter/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from forms import MyForm
 
@@ -24,8 +24,25 @@ def form():
     if request.method == 'POST' and form.validate_on_submit():
         username = form.username.data
         email = form.email.data
-        return render_template('success.html', username=username, email=email)
+
+        new_user = User(username=username, email=email) 
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect(url_for('success', username=username, email=email))
     return render_template('form.html', form=form)
+
+
+@app.route('/success')
+def success():
+    username = request.args.get('username')
+    email = request.args.get('email')
+    return render_template('success.html',username=username, email=email)
+
+@app.route('/users')
+def users():
+    all_users = User.query.all()
+    return render_template('users.html', users=all_users)
 
 if __name__ == '__main__':
     app.run(debug=True)
